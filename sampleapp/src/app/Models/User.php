b4f2271bcbe39836baa4ccd1ab4bf40c507ee793
kllaj <164129575+alfinkhalaj7@users.noreply.github.com>
2025-01-31 04:42:15 -1200

    Update User.php

--- sampleapp/src/app/Models/User.php
+++ sampleapp/src/app/Models/User.php
@@ -2,64 +2,24 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Filament\Models\Contracts\FilamentUser;
-use Filament\Models\Contracts\HasAvatar;
-use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable implements FilamentUser, HasAvatar
+class User extends Model
 {
-    use HasFactory, HasRoles, Notifiable;
+    use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
-        'avatar_url',
+        'name', 'email', 'password', 'level',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
-
-    public function getFilamentAvatarUrl(): ?string
+    public function karyaSeni()
     {
-        if ($this->avatar_url) {
-            return asset('storage/'.$this->avatar_url);
-        } else {
-            $hash = md5(strtolower(trim($this->email)));
-
-            return 'https://www.gravatar.com/avatar/'.$hash.'?d=mp&r=g&s=250';
-        }
+        return $this->hasMany(KaryaSeni::class);
     }
 
-    public function canAccessPanel(Panel $panel): bool
+    public function komentar()
     {
-        return true;
+        return $this->hasMany(Komentar::class);
     }
 }
